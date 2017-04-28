e7795a98f7d1383df69895c4ae08b2c112bf9ef7
Peter St.Jean <pstjean@mpr.org>
2017-04-28 11:39:50 -0500

    Adding accounts component gem

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  get 'home/show'
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'home#show'
+  mount ApmAccountsComponent::Engine => '/apm_accounts'
 end
