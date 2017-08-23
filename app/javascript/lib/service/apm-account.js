2824c0f38ffef78e226bf0451f2c6b2ea07f0848
Eileen Noonan <enoonan@mpr.org>
2017-08-23 10:39:52 -0500

    remove test auth code from compiled js

--- app/javascript/lib/service/apm-account.js
+++ app/javascript/lib/service/apm-account.js
@@ -1,15 +1,22 @@
 let apm_account
 
-if (typeof __TESTING__ !== 'undefined' && __TESTING__) {
-  apm_account_mock = {
-    get_token() {
-      return 'test_token'
+function makeAccount() {
+  /* develblock:start */
+  if (typeof __TESTING__ !== 'undefined' && __TESTING__) {
+    return {
+      get_token() {
+        return 'test_token'
+      }
     }
   }
-} else {
-  apm_account = new ApmAccount('/apm_accounts')
+  /* develblock:end */
+  let apm_account = new ApmAccount('/apm_accounts')
   if(!apm_account.is_logged_in()) {
     window.location.href = apm_account.log_in_path()
   }
+  return apm_account
 }
+
+apm_account = makeAccount()
+
 export default apm_account
