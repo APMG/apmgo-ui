7b79e71b7fb79519c2fb382edab9e4ae3dfd7d6d
Eileen Noonan <enoonan@mpr.org>
2017-09-14 10:46:12 -0500

    pre merge cleanup

--- app/javascript/lib/components/player/TimeScrubber.js
+++ app/javascript/lib/components/player/TimeScrubber.js
@@ -65,7 +65,6 @@ export default class TimeScrubberPresenter extends React.Component {
     return (
       <div>
         <input
-          id="timescrubber"
           type="range"
           max={this.props.duration}
           min={0}
